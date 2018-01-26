b9f73786dcaa661c6341eaceb674809c96b9ec5b
Stif Labarque <stifferson@outlook.com>
2018-01-26 20:40:18 +0100

    foto's aangepast

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+function carouselNormalization() {
+    var items = $('.carousel .carousel-item'), //grab all slides
+        heights = [], //create empty array to store height values
+        lowest; //create variable to make note of the shortest slide
+
+    if (items.length) {
+        function normalizeHeights() {
+            items.each(function () { //add heights to array
+                heights.push($(this).height());
+            });
+            lowest = Math.min.apply(null, heights); //cache lowest value
+            items.each(function (i, el) {
+                $(el).children().first().css('height', lowest + 'px');
+                $(el).children().first().css('margin', 'auto');
+            });
+        };
+        normalizeHeights();
+
+        $(window).on('resize orientationchange', function () {
+            lowest = 0, heights.length = 0; //reset vars
+
+            normalizeHeights(); //run it again 
+        });
+    }
+}
